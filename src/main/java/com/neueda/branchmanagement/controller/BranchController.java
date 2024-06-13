4a67dbe7c16848333e73d735888625e51a0233ac
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2024-06-13 14:59:09 -0500

    Added working Swaggar UI

--- src/main/java/com/neueda/branchmanagement/controller/BranchController.java
+++ src/main/java/com/neueda/branchmanagement/controller/BranchController.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping("/api/branches")
+@Tag(name = "Branch Management", description = "API for managing Bank branch details")
 public class BranchController {
 
     @Autowired
@@ -17,6 +21,7 @@ public class BranchController {
 
     // Get all branches
     @GetMapping
+    @Operation(summary = "Get all branches", description = "Retrieve a list of all branches")
     public ResponseEntity<List<Branch>> getAllBranches() {
         branchService.createBranch(new Branch("New York", "10001", "NY", "9AM-5PM", "John Doe", 10));
         branchService.createBranch(new Branch("Los Angeles", "90001", "CA", "10AM-6PM", "Jane Smith", 15));
@@ -28,6 +33,7 @@ public class BranchController {
 
     // Get a single branch by ID
     @GetMapping("/{id}")
+    @Operation(summary = "Get branch by ID", description = "Retrieve a branch by its ID")
     public ResponseEntity<Branch> getBranchById(@PathVariable Long id) {
         Branch branch = branchService.findBranchById(id);
         return branch != null ? new ResponseEntity<>(branch, HttpStatus.OK) :
@@ -36,6 +42,7 @@ public class BranchController {
 
     // Create a new branch
     @PostMapping
+    @Operation(summary = "Create a new branch", description = "Create a new branch")
     public ResponseEntity<Branch> createBranch(@RequestBody Branch branch) {
         Branch newBranch = branchService.createBranch(branch);
         return new ResponseEntity<>(newBranch, HttpStatus.CREATED);
@@ -43,6 +50,7 @@ public class BranchController {
 
     // Update an existing branch
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing branch", description = "Update an existing branch by its ID")
     public ResponseEntity<Branch> updateBranch(@PathVariable int id, @RequestBody Branch branchDetails) {
         Branch updatedBranch = branchService.updateBranch(id, branchDetails);
         return updatedBranch != null ? new ResponseEntity<>(updatedBranch, HttpStatus.OK) :
