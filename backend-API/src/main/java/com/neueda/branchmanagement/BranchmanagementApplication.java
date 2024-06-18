08a826ebdac8f0774f0e72a014bd63997727014a
Gabriel de Olaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2024-06-19 00:19:22 +0200

    Update BranchmanagementApplication.java

--- backend-API/src/main/java/com/neueda/branchmanagement/BranchmanagementApplication.java
+++ backend-API/src/main/java/com/neueda/branchmanagement/BranchmanagementApplication.java
@@ -2,15 +2,12 @@ package com.neueda.branchmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.neueda.branchmanagement.repository")
-@EntityScan("com.neueda.branchmanagement.model")
 public class BranchmanagementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BranchmanagementApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BranchmanagementApplication.class, args);
+	}
+
 }
