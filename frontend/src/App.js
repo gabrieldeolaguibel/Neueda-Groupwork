d163016b85b86d4bdc252b55c85c4fc993591213
RudyCab <caballero.rudy@utexas.edu>
2024-06-26 16:14:02 -0500

    added search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import NavBar from './components/NavBar';
 
 function App() {
   const [branches, setBranches] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
 
   useEffect(() => {
     fetch('http://localhost:8080/api/branches')
@@ -26,19 +28,32 @@ function App() {
     setBranches(branches.map(branch => (branch.id === updatedBranch.id ? updatedBranch : branch)));
   };
 
+    // Filter branches based on zip code
+    const filteredBranches = searchTerm
+    ? branches.filter(branch => branch.zip.includes(searchTerm))
+    : branches;
+
   return (
     <div className="App">
-      <NavBar onAddBranch={handleAddBranch} />
+      <NavBar onSearch={setSearchTerm} onAddBranch={handleAddBranch} />
       <header className="App-header">
         <div className="branch-list">
-          {branches.map(branch => (
+          {filteredBranches.map(branch => (
+              <BranchCard
+                key={branch.id}
+                branch={branch}
+                onDelete={handleDelete}
+                onUpdate={handleUpdateBranch}
+              />
+            ))}
+          {/* {branches.map(branch => (
             <BranchCard
               key={branch.id}
               branch={branch}
               onDelete={handleDelete}
               onUpdate={handleUpdateBranch}
             />
-          ))}
+          ))} */}
         </div>
       </header>
     </div>
